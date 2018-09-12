b8ec33505421251a67350615028bb363a0498042
root <cptdm2@gmail.com>
2018-09-12 12:54:36 +0000

    modified:   jenkins/inspect.sh

--- jenkins/inspect.sh
+++ jenkins/inspect.sh
@@ -1,13 +1,22 @@
+echo 'echo $PWD \n'
 echo $PWD
-ls $PWD/..
-ls /var/lib/jenkins/workspace
+echo 'ls $PWD/.. \n'
+echo 'ls $PWD/..
+echo 'ls /var/lib/jenkins/workspace
 ls /var/jenkins-home/workspace
-ls -Al /var/lib/jenkins/workspace/sonar-scanner_master-4NADIYUATQ5FQOWKFXMEYNOVVICA7URC46QNTF4BXCGRYNYG7RLA
+echo 'ls -Al /var/lib/jenkins/workspace/sonar-scanner_master-4NADIYUATQ5FQOWKFXMEYNOVVICA7URC46QNTF4BXCGRYNYG7RLA
+echo 'uname -a'
 uname -a
+echo 'whoami'
 whoami
+echo 'whereis jenkins || which jenkins'
 whereis jenkins || which jenkins
+echo 'cut -d: -f1 /etc/passwd'
 cut -d: -f1 /etc/passwd
+echo 'docker ps'
 docker ps
+echo 'cat /proc/1/cgroup'
 cat /proc/1/cgroup
+echo 'ps -L u n | tr -s ' ' | cut -d ' ' -f 2,3,14-'
 ps -L u n | tr -s ' ' | cut -d ' ' -f 2,3,14-
 
