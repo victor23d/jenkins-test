0c3d20691dbce98a56a998ec7aa9e2cd41f7dbe8
root <cptdm2@gmail.com>
2018-09-12 13:10:16 +0000

    modified:   jenkins/inspect.sh

--- jenkins/inspect.sh
+++ jenkins/inspect.sh
@@ -1,33 +1,39 @@
 echo 'echo $PWD'
 echo $PWD
-echo
-echo 'ls $PWD/..'
+echo '--------------------'
+echo 'ls -A $PWD/..'
 ls $PWD/..
-echo
-echo 'ls /var/lib/jenkins/workspace'
-ls /var/jenkins-home/workspace
-echo
+echo '--------------------'
+echo 'ls -A /var/lib/jenkins/workspace'
+ls /var/lib/jenkins/workspace
+echo '--------------------'
+echo 'ls -A /var/jenkins_home/workspace'
+ls /var/jenkins_home/workspace
+echo '--------------------'
 echo 'ls -Al /var/lib/jenkins/workspace/sonar-scanner_master-4NADIYUATQ5FQOWKFXMEYNOVVICA7URC46QNTF4BXCGRYNYG7RLA'
 echo 'uname -a'
 uname -a
-echo
+echo '--------------------'
 echo 'whoami'
 whoami
-echo
+echo '--------------------'
 echo 'whereis jenkins || which jenkins'
 whereis jenkins || which jenkins
-echo
+echo '--------------------'
 echo 'cut -d: -f1 /etc/passwd'
 cut -d: -f1 /etc/passwd
-echo
+echo '--------------------'
 echo 'docker ps'
 docker ps
-echo
+echo '--------------------'
 echo 'cat /proc/1/cgroup'
 cat /proc/1/cgroup
-echo
-echo 'ps -L u n | tr -s ' ' | cut -d ' ' -f 2,3,14-'
-ps -L u n | tr -s ' ' | cut -d ' ' -f 2,3,14-
-echo
+echo '--------------------'
+# Not available in alpine
+# echo 'ps -L u n | tr -s ' ' | cut -d ' ' -f 2,3,14-'
+# ps -L u n | tr -s ' ' | cut -d ' ' -f 2,3,14-
+echo 'ps -elf'
+ps -elf
+echo '--------------------'
 echo '\n'
 
